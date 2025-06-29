a499d0172bc82c734a22a00f3f40a1142560ed5c
ponnada-sowjanya <ponnadasowjanya4@gmail.com>
2025-06-29 14:49:02 +0530

    Fix image paths and imports

--- src/productContent.js
+++ src/productContent.js
@@ -1,3 +1,23 @@
+import img1 from './assets/img/jacketmens/img1.jpg';
+import img2 from './assets/img/jacketmens/img2.jpeg';
+import img3 from './assets/img/jacketmens/image3.webp';
+import img4 from './assets/img/jacketmens/image4.webp';
+
+import img5 from './assets/img/jacketwomen/img5.jpg';
+import img6 from './assets/img/jacketwomen/img6.jpg';
+import img7 from './assets/img/jacketwomen/image7.webp';
+import img8 from './assets/img/jacketwomen/imag8.webp';
+
+import img9 from './assets/img/shirtmen/img9.jpg';
+import img10 from './assets/img/shirtmen/img10.jpg';
+import img11 from './assets/img/shirtmen/img11.jpg';
+import img12 from './assets/img/shirtmen/img12.jpg';
+
+import img13 from './assets/img/shirtwomen/img13.jpg';
+import img14 from './assets/img/shirtwomen/image14.webp';
+import img15 from './assets/img/shirtwomen/image15.jpg';
+import img16 from './assets/img/shirtwomen/img16.webp';
+
 const products = [
     {
         id: 1,
@@ -6,7 +26,7 @@ const products = [
         category: "Mens Wear",
         description:
         "This versatile bomber jacket offers sleek style and lightweight comfort for any casual occasion. Perfect for layering, it's an essential for your daily wear.",
-        image: "/src/assets/img/jacketmens/img1.jpg",
+        image: img1,
     },
      {
         id: 2,
@@ -15,7 +35,7 @@ const products = [
         category: "Mens Wear",
         description:
         "Built for adventure, this durable utility jacket provides robust protection and practical pockets. Face the elements in rugged style and comfort.",
-        image: "/src/assets/img/jacketmens/img2.jpeg",
+        image: img2,
      },
      {
         id: 3,
@@ -24,7 +44,7 @@ const products = [
         category: "Mens Wear",
         description:
         "Crafted from genuine leather, this iconic biker jacket exudes edgy style and timeless appeal. Ride in confidence with its durable construction and classic fit.",
-        image: "/src/assets/img/jacketmens/image3.webp",
+        image: img3,
      },
       {
         id: 4,
@@ -33,7 +53,7 @@ const products = [
         category: "Mens Wear",
         description:
         "Stay warm without the bulk in this stylish and insulated puffer jacket. Its contemporary design and cozy fill make it perfect for chilly days.",
-       image: "/src/assets/img/jacketmens/image4.webp",
+       image: img4,
     },
     {
         id: 5,
@@ -42,7 +62,7 @@ const products = [
         category: "Women Wear",
         description:
         "This tailored blazer effortlessly elevates any outfit, perfect for office or casual smart looks. Its versatile design adds instant sophistication.",
-       image: "/src/assets/img/jacketwomen/img5.jpg",
+       image: img5,
     },
      {
         id: 6,
@@ -51,7 +71,7 @@ const products = [
         category: "Women Wear",
         description:
         "Stay incredibly warm and stylish in this lightweight, insulated puffer jacket. Designed for comfort, it's your go-to for chilly days",
-       image: "/src/assets/img/jacketwomen/img6.jpg",
+       image: img6,
     }, 
      {
         id: 7,
@@ -60,7 +80,7 @@ const products = [
         category: "Women Wear",
         description:
         "A timeless wardrobe staple, this classic denim jacket offers effortless cool and versatility. Layer it over anything for a casual, chic vibe.",
-       image: "/src/assets/img/jacketwomen/image7.webp",
+       image: img7,
     }, 
     {
         id: 8,
@@ -69,7 +89,7 @@ const products = [
         category: "Women Wear",
         description:
         "Add an edgy yet refined touch with this sleek faux leather moto jacket. It's perfect for adding a touch of rebellion and style to your ensemble.",
-       image: "/src/assets/img/jacketwomen/imag8.webp",
+       image: img8,
     }, 
     {
         id: 9,
@@ -78,7 +98,7 @@ const products = [
         category: "Men Shirt",
         description:
         "A wardrobe essential, this versatile Oxford shirt offers timeless style and comfort. Perfect for both casual and smart-casual occasions.",
-       image: "/src/assets/img/shirtmen/img9.jpg",
+       image: img9,
     },
      {
         id: 10,
@@ -87,7 +107,7 @@ const products = [
         category: "Men Shirt",
         description:
        "Stay cool and relaxed in this lightweight linen blend shirt, ideal for warm weather. Its breathable fabric ensures all-day comfort and a laid-back look.",
-       image: "/src/assets/img/shirtmen/img10.jpg",
+       image: img10,
     }, 
     {
         id: 11,
@@ -96,7 +116,7 @@ const products = [
         category: "Men Shirt",
         description:
         "Achieve a polished look with this wrinkle-resistant dress shirt, designed for professional settings. It provides crisp style and effortless elegance all day.",
-       image: "/src/assets/img/shirtmen/img11.jpg",
+       image: img11,
     }, 
      {
         id: 12,
@@ -105,7 +125,7 @@ const products = [
         category: "Men Shirt",
         description:
         "Express your personality with this comfortable and stylish graphic tee. It's the perfect casual choice for everyday wear and relaxed outings.",
-       image: "/src/assets/img/shirtmen/img12.jpg",
+       image: img12,
     },  
      {
         id: 13,
@@ -114,7 +134,7 @@ const products = [
         category: "Women Shirt",
         description:
         "This versatile blouse offers a relaxed yet polished look for any casual or semi-formal occasion. Its comfortable fit makes it perfect for all-day wear.",
-       image: "/src/assets/img/shirtwomen/img13.jpg",
+       image: img13,
     }, 
     {
         id: 14,
@@ -123,7 +143,7 @@ const products = [
         category: "Women Shirt",
         description:
         "Drape yourself in luxury with this delicate silk camisole, ideal for layering or solo wear. Its smooth texture adds a touch of sophisticated allure.",
-       image: "/src/assets/img/shirtwomen/image14.webp",
+       image: img14,
     },  
      {
         id: 15,
@@ -132,7 +152,7 @@ const products = [
         category: "Women Shirt",
         description:
         "A timeless essential, this classic cotton button-up provides sharp style for professional or weekend wear. It's a foundational piece for any wardrobe.",
-       image: "/src/assets/img/shirtwomen/image15.jpg",
+       image: img15,
     }, 
      {
         id: 16,
@@ -141,7 +161,7 @@ const products = [
         category: "Women Shirt",
         description:
         "Embrace relaxed style with this flowy tunic top, featuring unique prints and comfortable fabric. Perfect for a bohemian-inspired look or casual outings.",
-       image: "/src/assets/img/shirtwomen/img16.webp",
+       image: img16,
     },   
 
 ];
